867c7b21cc5bea34c7a1c5662130826412c93de6
alpacaPwaa <doromaljhonrey@gmail.com>
2026-06-11 11:17:54 +0800

    feat: implement initial user and student management consolidation

--- app/[locale]/(protected)/admin/students/page.client.tsx
+++
@@ -1,7 +0,0 @@
-"use client";
-
-import { AdminStudentsPageClient } from "@/components/admin/student-management/admin-students-page-client";
-
-export function PageClient() {
-  return <AdminStudentsPageClient />;
-}
